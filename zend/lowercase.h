0724086e9af8303f3c6e5cf39fc356d7a2f8b0ee
Emiel Bruijntjes <emiel.bruijntjes@copernica.com>
2016-10-13 22:27:58 +0200

    work in progress on fixing the Value::isCallable method

---
+++ zend/lowercase.h
@@ -0,0 +1,80 @@
+/**
+ *  LowerCase.h
+ *
+ *  Class to temporary convert a name to lowercase
+ *
+ *  @author Emiel Bruijntjes <emiel.bruijntjes@copernica.com>
+ *  @copyright 2016 Copernica BV
+ */
+
+/**
+ *  Include guard
+ */
+#pragma once
+
+/**
+ *  Begin of namespace
+ */
+namespace Php {
+
+/**
+ *  Class definition
+ */
+class LowerCase
+{
+private:
+    /**
+     *  The lowercase name
+     *  @var char *
+     */
+    const char *_name;
+
+public:
+    /**
+     *  Constructor
+     *  @param  name        the original name
+     */
+    LowerCase(const char *name) : 
+        _name(zend_str_tolower_dup(name)) {}
+        
+    /**
+     *  No copy'ing or moving
+     *  @param  that
+     */
+    LowerCase(const LowerCase &that) = delete;
+    LowerCase(LowerCase &&that) = delete;
+
+    /**
+     *  Destructor
+     */
+    virtual ~LowerCase()
+    {
+        // release the data
+        efree(_name);
+    }
+    
+    /**
+     *  Expose internal value
+     *  @return const char *
+     */
+    const char *value()
+    {
+        // expose member
+        return _name;
+    }
+    
+    /**
+     *  Cast to a const char *
+     *  @return const char *
+     */
+    operator const char * () 
+    {
+        // expose member
+        return _name;
+    }
+};
+
+/**
+ *  End of namespace
+ */
+}
--- zend/value.cpp
+++ zend/value.cpp
@@ -844,14 +844,45 @@ Value Value::operator()() const
  */
 bool Value::isCallable(const char *name)
 {
-    // wrap the name in a Php::Value object to get a zval
-    Value method(name);
+    // this only makes sense if we are an object
+    if (!isObject()) return false;
+    
+    // get the class properties
+    zend_class_entry *ce = Z_OBJCE_P(_val);
+    
+    // length of the name
+    size_t len = strlen(name);
 
-    // we need the tsrm_ls variable
-    TSRMLS_FETCH();
+    // convert name to lowercase
+    LowerCase methodname(name);
+    
+    // check if thus function exists
+    if (zend_hash_exists(&ce->function_table, methodname, len+1)) return true;
+    
+    // can we dynamically fetch the method?
+    if (Z_OBJ_HT_P(_val)->get_method == nullptr) return false;
+    
+    // get the function
+    union _zend_function *func = Z_OBJ_HT_P(_val)->get_method(&Z_OBJ_P(_val), methodname, nullptr);
+    
+    // if function does not exist, we do not have to check further
+    if (func == nullptr) return false;
 
-    // ask zend nicely whether the function is callable
-    return zend_is_callable_ex(method._val, _val, IS_CALLABLE_CHECK_NO_ACCESS, nullptr, nullptr, nullptr, nullptr TSRMLS_CC);
+    // is it a special internal function, otherwise it is not callable
+    if (func->type != ZEND_INTERNAL_FUNCTION) return false;
+
+    // copied from zend_builtin_functions.c
+    if ((func->common.fn_flags & ZEND_ACC_CALL_VIA_HANDLER) == 0) return false;
+
+    // Returns true to the fake Closure's __invoke
+    bool result = func->common.scope == zend_ce_closure && (len == sizeof(ZEND_INVOKE_FUNC_NAME)-1) && memcmp(methodname, ZEND_INVOKE_FUNC_NAME, sizeof(ZEND_INVOKE_FUNC_NAME)-1) == 0;
+        
+    // clean resources
+    efree((char*)((zend_internal_function*)func)->function_name);
+    efree(func);
+    
+    // done
+    return result;
 }
 
 /**
